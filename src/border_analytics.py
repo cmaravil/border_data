6b2884aefdcc94de8b1e2b721be38db63427a4b3
Catalina <maravilla.catalina@gmail.com>
2019-11-20 18:29:32 -0500

    added comments

--- src/border_analytics.py
+++ src/border_analytics.py
@@ -7,10 +7,25 @@ from dateutil.parser import parse
 from decimal import Decimal, ROUND_UP
 
 def main(input_file, output_file):
+    """
+    Reads the input file, calculates sum and average, then appends everything to an output  as a csv file.
+
+
+    Args:
+        input_file
+        output_file
+    Returns:
+        None
+        The results are stored to the ouput csv.
+    """
+
+    # initializing variables
     results = []
     sums = {}
     avg = {}
 
+    # reads the input csv and calculates the sum and populates the sums dictionary
+    # also the result array keeps csv input data
     with open(input_file) as csv_file:
         csv_reader = list(csv.reader(csv_file, delimiter=','))
         header = csv_reader.pop(0)
@@ -29,10 +44,12 @@ def main(input_file, output_file):
         if denominator == 0:
             avg[key] = 0
         else:
+            # sums the results of the previous months upto but not including current month and divides by that number
+            # using range from 1 upto current month, this yields an array of 1...month - 1
             avg[key] = Decimal(sum([sums.get("{},{},{}".format(border, m, measure),0)
                                   for m in range(1, int(month))]) / denominator).quantize(0, ROUND_UP)
 
-                                  
+    #writing and formating to output file csv                  
     with open(output_file, mode='w') as results_csv:
         results_csv.writelines("Border,Date,Measure,Value,Average\n")
         for key in sums.keys():
@@ -44,8 +61,9 @@ def main(input_file, output_file):
 
 
 if __name__ == "__main__":
+
     if len(sys.argv) < 3:
-        print("To run the progrm you need the input and the output as args\n example: python src/border_analytics.py path/input path/output")
+        print("To run the progrm you need the input and the output as args\n example: python ./src/border_analytics.py ./path/input ./path/output")
         sys.exit(1)
 
     main(sys.argv[1], sys.argv[2])
